a76eecea627f5540246d63e69532fee9d37c11ac
tina3008 <kicja_3@ukr.net>
2024-01-17 18:47:16 +0200

    3

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,13 @@
 'use strict';
-function makeTransaction(quantity, pricePerDroid){
-    return `You ordered ${quantity} droids worth ${pricePerDroid*quantity} credits!`;
+function slugify(title){
+   const normalizedMessage = title.toLowerCase()
+   const array = normalizedMessage.split(" ")
+   const slug = array.join("-")
+   return slug
+    
 }
 
-console.log(makeTransaction(5, 3000));
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500));
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
